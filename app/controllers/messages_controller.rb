226dcb3b7e4d06566e62b603728429ea63314cf6
Sebastian <sebastianplagemann@gmail.com>
2018-01-25 13:41:45 +0100

    Add verification

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,16 @@
 class MessagesController < ApplicationController
+  def index
+    if hub_params.fetch('hub.mode', '') == 'subscribe' &&
+       hub_params.fetch('hub.verify_token', '') == ENV.fetch('VERIFY_TOKEN')
+      render plain: hub_params.fetch('hub.challenge', '')
+    else
+      render plain: ''
+    end
+  end
+
+  private
+
+  def hub_params
+    params.permit('hub.challenge', 'hub.mode', 'hub.verify_token')
+  end
 end
