09b1480cd8b67004428d9377f473147f9ef9442b
Sebastian <sebastianplagemann@gmail.com>
2018-01-25 15:26:10 +0100

    Add answer

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,21 @@
+# frozen_string_literal: true
+
 class MessagesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  before_action def log_request_headers
+    return unless Rails.logger.debug?
+    Rails.logger.debug '  Headers:'
+    request.headers
+           .to_h
+           .select { |name, _| name.in?(ActionDispatch::Http::Headers::CGI_VARIABLES) || name =~ /^HTTP_/ }
+           .transform_keys { |name| name.sub(/^HTTP_/, '').downcase.dasherize.gsub(/\b\w/, &:upcase) }
+           .sort_by { |name, _| name }
+           .each do |name, value|
+      Rails.logger.debug "    #{name}: #{value}"
+    end
+  end
+
   def index
     if hub_params.fetch('hub.mode', '') == 'subscribe' &&
        hub_params.fetch('hub.verify_token', '') == ENV.fetch('VERIFY_TOKEN')
@@ -6,6 +23,24 @@ class MessagesController < ApplicationController
     else
       render plain: ''
     end
+  end
+
+  def handle_message
+    msg = {
+      recipient: {
+        id: params[:sender][:id]
+      },
+      message: {
+        text: params[:message][:text]
+      }
+    }
+
+    url = ENV.fetch('API_URL')
+    token = ENV.fetch('TOKEN')
+    httparty.post url + token,
+                  body: msg.to_json,
+                  headers: { 'Content-Type' => 'application/json' }
+    render plain: ''
   end
 
   private
